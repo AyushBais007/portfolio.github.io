b6eeb9d543367455b4d27cd9011e28aa0c455149
AyushBais007 <ayushbais167@gmail.com>
2023-01-10 22:39:58 +0530

    updated features

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,8 +6,8 @@ const navMenu = document.getElementById('sidebar'),
 
 /*===== SIDEBAR SHOW =====*/
 /* Validate If Constant Exists */
-if(navToggle){
-    navToggle.addEventListener('click',()=>{
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
         console.log('clicked-sowsidebar')
         navMenu.classList.add('show-sidebar')
     })
@@ -15,32 +15,32 @@ if(navToggle){
 
 /*===== SIDEBAR HIDDEN =====*/
 /* Validate If Constant Exists */
-if(navClose){
-    navClose.addEventListener('click',()=>{
-       navMenu.classList.remove('show-sidebar')
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-sidebar')
     })
 }
 
 /*=============== SKILLS TABS ===============*/
 const tabs = document.querySelectorAll('[data-target]'),
-      tabContent = document.querySelectorAll('[data-content]')
+    tabContent = document.querySelectorAll('[data-content]')
 
 
-      tabs.forEach(tab=>{
-        tab.addEventListener('click',()=>{
-            const target = document.querySelector(tab.dataset.target)
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
 
-            tabContent.forEach(tabContents=>{
-                tabContents.classList.remove("skills__active")
-            })
-            target.classList.add("skills__active")
-
-            tabs.forEach(tab=>{
-                tab.classList.remove("skills__active")
-            })
-            tab.classList.add("skills__active")
+        tabContent.forEach(tabContents => {
+            tabContents.classList.remove("skills__active")
         })
-      })
+        target.classList.add("skills__active")
+
+        tabs.forEach(tab => {
+            tab.classList.remove("skills__active")
+        })
+        tab.classList.add("skills__active")
+    })
+})
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
 var mixer = mixitup('.work__container', {
@@ -55,33 +55,33 @@ var mixer = mixitup('.work__container', {
 
 const linkWork = document.querySelectorAll('.work__item')
 
-function activeWork(){
-    linkWork.forEach(l=>{
+function activeWork() {
+    linkWork.forEach(l => {
         l.classList.remove('active__work')
         this.classList.add('active__work')
     })
 }
 
-linkWork.forEach(l=>{ 
+linkWork.forEach(l => {
     l.addEventListener('click', activeWork)
 })
 /*===== Work Popup =====*/
 
-document.addEventListener('click',(e)=>{
-    if(e.target.classList.contains("work__button")){
+document.addEventListener('click', (e) => {
+    if (e.target.classList.contains("work__button")) {
         togglePortfolioPopup();
         portfolioItemDetails(e.target.parentElement)
     }
 })
 
-function togglePortfolioPopup(){
-   // console.log(document.querySelector('.portfolio__popup').classList.toggle('.open'))
+function togglePortfolioPopup() {
+    // console.log(document.querySelector('.portfolio__popup').classList.toggle('.open'))
     document.querySelector(".portfolio__popup").classList.toggle("open");
 }
 
-document.querySelector(".portfolio__popup-close").addEventListener("click",togglePortfolioPopup);
+document.querySelector(".portfolio__popup-close").addEventListener("click", togglePortfolioPopup);
 
-function portfolioItemDetails(portfolioItem){
+function portfolioItemDetails(portfolioItem) {
     document.querySelector(".pp__thumbnail img").src = portfolioItem.querySelector(".work__img").src;
     document.querySelector(".portfolio__popup-subtitle span").innerHTML = portfolioItem.querySelector(".work__title").innerHTML;
     document.querySelector(".portfolio__popup-body").innerHTML = portfolioItem.querySelector(".portfolio__item-details").innerHTML
@@ -94,43 +94,88 @@ function portfolioItemDetails(portfolioItem){
 
 /*=============== INPUT ANIMATION ===============*/
 const inputs = document.querySelectorAll(".input");
-function focusFunc(){
+function focusFunc() {
     let parent = this.parentNode;
     parent.classList.add('focus');
 }
 
-function blurFunc(){
+function blurFunc() {
     let parent = this.parentNode;
-    if(this.value == ''){
+    if (this.value == '') {
         parent.classList.remove('focus');
     }
 }
 
-inputs.forEach((input)=>{
-    input.addEventListener('focus',focusFunc);
-    input.addEventListener('blur',blurFunc);
-    
+inputs.forEach((input) => {
+    input.addEventListener('focus', focusFunc);
+    input.addEventListener('blur', blurFunc);
+
 })
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]');
 
-window.addEventListener('scroll',navHighlighter);
+window.addEventListener('scroll', navHighlighter);
 
-function navHighlighter(){
+function navHighlighter() {
     let scrollY = window.pageYOffset;
-    sections.forEach(current=>{
+    sections.forEach(current => {
         const sectionHeight = current.offsetHeight;
         const sectionTop = current.offsetTop - 50,
-        sectionId = current.getAttribute('id');
+            sectionId = current.getAttribute('id');
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
-            
+
         }
-        else{
+        else {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
         }
         navMenu.classList.remove('show-sidebar');
     })
 }
 /*=============== SHOW SCROLL UP ===============*/
+sendButton = document.getElementById('sendButton');
+sendButton.addEventListener('click', () => {
+    console.log('send messasge')
+    const email = document.getElementById('email');
+    const name = document.getElementById('name');
+    const message = document.getElementById('message')
+  
+    if(email.value != ''){
+        sendEmail(email.value,name.value,message.value);
+    }
+    console.log(email.value,name.value,message.value)
+})
+
+function sendEmail(email,name,message) {
+    // console.log(Email)
+    // Email.send({
+    //   Host: "smtp.elasticemail.com",
+    //   Username: "dakshgamer167@gmail.com",
+    //   Password: "E6B2FC1793EC151C2BE62C0E4FC2159A7580",
+    //   To: 'ayushbais167@gmail.com',
+    //   From: "ayushbais167@gmail.com",
+    //   Subject: "Sending Email using javascript",
+    //   Body: "Well that was easy!!",
+    // })
+    // .then(function (message) {
+    //     console.log(message)
+    //     alert(message)
+    //   });
+
+    var templateParams = {
+        name: name,
+        email:email,
+        message: message
+    };
+     
+    emailjs.send('service_awkdjvi', 'template_xx4hgo6', templateParams)
+        .then(function(response) {
+            alert('message sent successfuuly');
+            location.reload()
+
+           console.log('SUCCESS!', response.status, response.text);
+        }, function(error) {
+           console.log('FAILED...', error);
+        });
+}
